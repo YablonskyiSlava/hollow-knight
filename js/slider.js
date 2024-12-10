295fc61fcf350e89deefa61d8e5bd5bb06617baa
YablonskyiSlava <slavasyper615@gmail.com>
2024-12-10 17:05:08 +0200

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,27 @@
+// $('.multiple-items').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 3
+//   });
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
